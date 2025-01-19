b9c8bde6b5ce3ceaaf548c159b4355bdc4860217
Melonchanism <121840186+Melonchanism@users.noreply.github.com>
2025-01-18 21:05:17 -0800

    Fix menu offset by 1px

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # TI 84 ClockE
 
-My first program to have been made for the TI 84+ CE in C
-Has both a digital and analog section
-I mean, what is it other than a clock?
+My first program to have been made for the TI 84+ CE in C <br>
+Has both a digital and analog section <br>
+I mean, what is it other than a clock? <br>
--- src/menu.h
+++ src/menu.h
@@ -7,14 +7,14 @@ void drawMenu(uint8_t style) {
   fontlib_SetTransparency(false);
   fontlib_SetFont(glyph, 0);
   fontlib_SetColors(148, 0);
-  fontlib_SetCursorPosition(2, 221);
+  fontlib_SetCursorPosition(2, 222);
   // Text is weird to bypass required characters in fontlibc
   fontlib_DrawString("01120112");
   fontlib_SetFont(system, 0);
   if (style == 1) {
     fontlib_SetColors(0, 148);
   }
-  fontlib_SetCursorPosition(10, 224);
+  fontlib_SetCursorPosition(10, 225);
   // DIGI
   fontlib_DrawString("0121");
   if (style == 2) {
@@ -22,7 +22,7 @@ void drawMenu(uint8_t style) {
   } else {
     fontlib_SetColors(148, 0);
   }
-  fontlib_SetCursorPosition(73, 224);
+  fontlib_SetCursorPosition(73, 225);
   // ANLG
   fontlib_DrawString("3452");
   gfx_BlitBuffer();
